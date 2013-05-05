be7cc99c45d27af89c3482e9c3fe39b1ca809dfa
Shish <shish@shishnet.org>
2013-05-05 20:56:44 +0100

    interactive mode

--- chunker/main.py
+++ chunker/main.py
@@ -7,15 +7,15 @@ import os
 from glob import glob
 
 from chunker.repo import Repo
-from chunker.util import get_config_path
+from chunker.util import get_config_path, heal, log
 
 
-def chunk(args):
+def cmd_chunk(args):
     r = Repo(args.output, args.input, args.name or os.path.basename(args.input))
     r.save(args.output, state=False)
 
 
-def add(args):
+def cmd_add(args):
     r = Repo(
         args.filename,
         args.directory,
@@ -24,24 +24,25 @@ def add(args):
     r.save_state()
 
 
-def heal(args):
+def cmd_heal(args):
     known = []
     missing = []
     for filename in glob(get_config_path("*.state")):
         r = Repo(filename)
         known.extend(r.get_known_chunks())
         missing.extend(r.get_missing_chunks())
-    r.self_heal(known_chunks=known, missing_chunks=missing)
+    saved = heal(known, missing)
+    return {"status": "ok", "saved": saved}
 
 
-def fetch(args):
+def cmd_fetch(args):
     repos = []
     for filename in glob(get_config_path("*.state")):
         repos.append(Repo(filename))
-    print repos
+    log("Repos: %s" % ", ".join(["%r" % repo for repo in repos]))
 
 
-def stat(args):
+def cmd_stat(args):
     r = Repo(args.filename, args.directory, os.path.basename(args.filename))
     chunks = r.get_known_chunks()
     seen = {}
@@ -61,36 +62,52 @@ def stat(args):
 
 
 def main():
-    parser = argparse.ArgumentParser(description="a thing")
+    args = sys.argv[1:]
+    if args:
+        do(args)
+    else:
+        while True:
+            cmd = raw_input("chunker> ")
+            try:
+                print do(cmd.split())
+            except Exception as e:
+                print {"status": "error", "message": str(e)}
+
+class NonExitingArgumentParser(argparse.ArgumentParser):
+    def exit(self, status=0, message=None):
+        raise Exception(message)
+
+def do(argv):
+    parser = NonExitingArgumentParser(description="a thing")
     subparsers = parser.add_subparsers()
 
     p_chunk = subparsers.add_parser("chunk")
     p_chunk.add_argument("output")
     p_chunk.add_argument("input")
     p_chunk.add_argument("--name")
-    p_chunk.set_defaults(func=chunk)
+    p_chunk.set_defaults(func=cmd_chunk)
 
     p_heal = subparsers.add_parser("heal")
     #p_heal.add_argument("filename")
     #p_heal.add_argument("directory")
-    p_heal.set_defaults(func=heal)
+    p_heal.set_defaults(func=cmd_heal)
 
     p_add = subparsers.add_parser("add")
     p_add.add_argument("--name")
     p_add.add_argument("filename")
     p_add.add_argument("directory")
-    p_add.set_defaults(func=add)
+    p_add.set_defaults(func=cmd_add)
 
     p_fetch = subparsers.add_parser("fetch")
-    p_fetch.set_defaults(func=fetch)
+    p_fetch.set_defaults(func=cmd_fetch)
 
     p_stat = subparsers.add_parser("stat")
     p_stat.add_argument("filename")
     p_stat.add_argument("directory")
-    p_stat.set_defaults(func=stat)
+    p_stat.set_defaults(func=cmd_stat)
 
-    args = parser.parse_args()
-    args.func(args)
+    args = parser.parse_args(argv)
+    return args.func(args)
 
 if __name__ == "__main__":
     sys.exit(main())
